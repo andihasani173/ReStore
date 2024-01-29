3f6e73faacb8cb348cddfdc656a3e111b9842a9d
Andi <andihasani173@gmail.com>
2024-01-29 13:19:59 +0100

    First commit

---
+++ API/Data/StoreContext.cs
@@ -0,0 +1,19 @@
+
+
+using System.ComponentModel;
+using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+
+    }
+}
