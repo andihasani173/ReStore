56522f934a4f732b7665b39da41bad82afc6e26a
Andi <andihasani173@gmail.com>
2024-02-13 15:18:49 +0100

    End of Section 3

---
+++ client/src/app/models/product.ts
@@ -0,0 +1,11 @@
+
+export interface Product {
+    id: number,
+    name: string,
+    description: string,
+    pictureUrl: string,
+    price: number,
+    brand: string,
+    type?: string,
+    quantityInStock?: number
+}
