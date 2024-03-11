ae9e350c7c2cad9a24c55bfeb58afab54656f991
Andi <andihasani173@gmail.com>
2024-03-11 09:32:11 +0100

    End of section 5 A.H

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -7,7 +7,7 @@ namespace API.Controllers;
 
 [ApiController]
 [Route("api/[controller]")]
-public class ProductsController : ControllerBase
+public class ProductsController : BaseApiController
 {
     private readonly StoreContext _context;
     public ProductsController(StoreContext context)
@@ -25,7 +25,11 @@ public class ProductsController : ControllerBase
     [HttpGet("{id}")]
     public async Task<ActionResult<Product>> GetProduct(int id)
     {
-        return await _context.Products.FindAsync(id);
+        var product = await _context.Products.FindAsync(id);
+
+        if (product == null) return NotFound();
+
+        return product;
     }
 }
 
--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,39 +1,34 @@
-import { useState } from 'react'
-import { Product } from '../models/product';
-import Catalog from '../../features/catalog/Catalog';
-import { Container, CssBaseline, ThemeProvider, createTheme,  } from '@mui/material';
+import { Container, CssBaseline, ThemeProvider, createTheme } from '@mui/material';
 import Header from './Header';
+import { useState } from 'react';
 import { Outlet } from 'react-router-dom';
-
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
-  const [darkMode , setDarkMode] = useState(false);
-  const paletteType = darkMode ? 'dark' : 'light';
+  const [darkMode, setDarkMode] = useState(false);
+  const palleteType = darkMode ? 'dark' : 'light';
   const theme = createTheme({
-    palette:{
-      mode: paletteType,  
-      background:{
-        default: paletteType ==='light' ? '#eaeaea' : '#121212'
+    palette: {
+      mode: palleteType,
+      background: {
+        default: (palleteType === 'light') ? '#eaeaea' : '#121212'
       }
     }
   })
- 
+  function handleThemeChange() {
+    setDarkMode(!darkMode);
+  }
 
-function handleThemeChange(){
-  setDarkMode(!darkMode)
+  return (
+    <ThemeProvider theme={theme}>
+      <ToastContainer position='bottom-right' hideProgressBar theme='colored'  />
+      <CssBaseline />
+      <Header darkMode={darkMode} handleThemeChange={handleThemeChange} />
+      <Container>
+        <Outlet />
+      </Container>
+    </ThemeProvider>
+  );
 }
-
-return (
-  <ThemeProvider theme={theme}>
-  <CssBaseline/>
-    <Header darkMode={darkMode} handleThemeChange={handleThemeChange} />
-    <Container>
-    <Outlet/>
-    </Container>
-     
-  </ThemeProvider>
-);
-
-}
-
-export default App;
+export default App
